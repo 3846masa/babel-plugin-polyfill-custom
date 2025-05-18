229e7faa19040b2f9155011a5bdd5fb49c0123ac
3846masa <3846masahiro+git@gmail.com>
2025-05-19 00:10:03 +0900

    chore: fix types

--- src/types/mdn__browser-compat-data.d.ts
+++
@@ -1,5 +0,0 @@
-import type { CompatData } from '@mdn/browser-compat-data/types';
-const bcd: CompatData;
-/* eslint-disable-next-line import/no-unresolved */
-export * from '@mdn/browser-compat-data/types';
-export default bcd;
